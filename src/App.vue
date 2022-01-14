dff5cecaf1e85ed8c7aa3b6057ba7dda5ff97720
Happy Dev <happyguydev@gmail.com>
2022-01-14 21:50:40 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+      <CalendarAppointment/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import CalendarAppointment from './components/CalendarAppointment';
+
+export default {
+  name: 'App',
+
+  components: {
+    CalendarAppointment,
+  },
+};
+</script>
