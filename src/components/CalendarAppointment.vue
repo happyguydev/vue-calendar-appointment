31f87d95deccd007f3c5ec641f9c156f0c42f63a
Happy Dev <happyguydev@gmail.com>
2022-01-18 06:30:54 +0900

    First upload

--- src/components/CalendarAppointment.vue
+++ src/components/CalendarAppointment.vue
@@ -1,128 +1,337 @@
 <template>
-  <v-row class="fill-height">
-    <v-col>
-      <v-sheet height="64">
-        <v-toolbar
-            flat
-        >
-          <v-btn
-              outlined
-              class="mr-4"
-              color="grey darken-2"
-              @click="setToday"
-          >
-            Today
-          </v-btn>
-          <v-btn
-              fab
-              text
-              small
-              color="grey darken-2"
-              @click="prev"
-          >
-            <v-icon small>
-              mdi-chevron-left
-            </v-icon>
-          </v-btn>
-          <v-btn
-              fab
-              text
-              small
-              color="grey darken-2"
-              @click="next"
-          >
-            <v-icon small>
-              mdi-chevron-right
-            </v-icon>
-          </v-btn>
-          <v-toolbar-title v-if="$refs.calendar">
-            {{ $refs.calendar.title }}
-          </v-toolbar-title>
-          <v-spacer></v-spacer>
-        </v-toolbar>
-      </v-sheet>
-      <v-sheet height="600">
-        <v-calendar
-            ref="calendar"
-            v-model="focus"
-            color="primary"
-            type="category"
-            category-show-all
-            :event-overlap-mode="mode"
-            :categories="categories"
-            :events="events"
-            :event-color="getEventColor"
-            @change="fetchEvents"
-        ></v-calendar>
-      </v-sheet>
-    </v-col>
-  </v-row>
+  <div style="padding: 10px">
+    <v-row class="fill-height">
+      <v-col cols="9">
+        <vue-cal today-button
+                 hide-view-selector
+                 sticky-split-labels
+                 active-view="day"
+                 ref="vuecal"
+                 :time-from="0"
+                 :time-to="24 * 60"
+                 :time-step="15"
+                 :time-cell-height="30"
+                 :events="events"
+                 :split-days="splitUsers"
+                 :selected-date="selectedDate"
+                 :disable-views="['years', 'year', 'month', 'week']"
+                 :drag-to-create-event="false"
+                 :cell-click-hold="false"
+                 :min-split-width="150"
+                 @cell-dblclick="onEventCreate">
+        </vue-cal>
+      </v-col>
+      <v-col cols="3">
+        <vue-cal class="vuecal--blue-theme vuecal--rounded-theme"
+                 style="max-width: 270px;height: 290px"
+                 active-view="month"
+                 xsmall
+                 hide-view-selector
+                 :time="false"
+                 :disable-views="['years', 'year', 'week', 'day']"
+                 @cell-focus="selectedDate = $event">
+        </vue-cal>
+        <p class="mt-10">Resource</p>
+        <div v-for="(value, name, index) in splitUsers" v-bind:key="index">
+          <v-checkbox
+              v-model="selectedResource"
+              :label="`${value.label}`"
+              :value="value.id"
+          ></v-checkbox>
+        </div>
+      </v-col>
+    </v-row>
+    <v-dialog v-model="showEventCreationDialog" :persistent="true" max-width="800">
+      <v-card>
+        <v-card-title>
+          New Appointment
+        </v-card-title>
+        <v-card-text>
+          <v-text-field
+              v-model="patientName"
+              label="Patient Name"
+              placeholder="Patient Name"
+              :rules="nameRules"
+              required
+          />
+          <v-row>
+            <v-col
+                class="d-flex align-center"
+                cols="12"
+                md="2"
+            >
+              <span>Start Time: </span>
+            </v-col>
+            <v-col
+                cols="12"
+                md="5"
+            >
+              <v-menu
+                  ref="startDateMenu"
+                  v-model="startDateMenu"
+                  :close-on-content-click="false"
+                  transition="scale-transition"
+                  offset-y
+                  max-width="290px"
+                  min-width="auto"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                      v-model="startDateFormatted"
+                      label="Date"
+                      persistent-hint
+                      prepend-icon="mdi-calendar"
+                      v-bind="attrs"
+                      @blur="startDate = parseDate(startDateFormatted)"
+                      v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                    v-model="startDate"
+                    no-title
+                    @input="startDateMenu = false"
+                ></v-date-picker>
+              </v-menu>
+            </v-col>
+
+            <v-col
+                cols="12"
+                md="5"
+            >
+              <v-menu
+                  ref="startTimeMenu"
+                  v-model="startTimeModel"
+                  :close-on-content-click="false"
+                  :nudge-right="40"
+                  :return-value.sync="startTime"
+                  transition="scale-transition"
+                  offset-y
+                  max-width="290px"
+                  min-width="290px"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                      v-model="startTime"
+                      label="Time"
+                      prepend-icon="mdi-clock-time-four-outline"
+                      readonly
+                      v-bind="attrs"
+                      v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-time-picker
+                    v-if="startTimeModel"
+                    v-model="startTime"
+                    full-width
+                    @click:minute="$refs.startTimeMenu.save(startTime)"
+                ></v-time-picker>
+              </v-menu>
+            </v-col>
+          </v-row>
+          <v-row class="mt-0">
+            <v-col
+                class="d-flex align-center"
+                cols="12"
+                md="2"
+            >
+              <span>End Time: </span>
+            </v-col>
+            <v-col
+                cols="12"
+                md="5"
+            >
+              <v-menu
+                  ref="endDateMenu"
+                  v-model="endDateMenu"
+                  :close-on-content-click="false"
+                  transition="scale-transition"
+                  offset-y
+                  max-width="290px"
+                  min-width="auto"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                      v-model="endDateFormatted"
+                      label="Date"
+                      persistent-hint
+                      prepend-icon="mdi-calendar"
+                      v-bind="attrs"
+                      @blur="endDate = parseDate(endDateFormatted)"
+                      v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                    v-model="endDate"
+                    no-title
+                    @input="endDateMenu = false"
+                ></v-date-picker>
+              </v-menu>
+            </v-col>
+
+            <v-col
+                cols="12"
+                md="5"
+            >
+              <v-menu
+                  ref="endTimeMenu"
+                  v-model="endTimeModel"
+                  :close-on-content-click="false"
+                  :nudge-right="40"
+                  :return-value.sync="endTime"
+                  transition="scale-transition"
+                  offset-y
+                  max-width="290px"
+                  min-width="290px"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                      v-model="endTime"
+                      label="Time"
+                      prepend-icon="mdi-clock-time-four-outline"
+                      readonly
+                      v-bind="attrs"
+                      v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-time-picker
+                    v-if="endTimeModel"
+                    v-model="endTime"
+                    full-width
+                    @click:minute="$refs.endTimeMenu.save(endTime)"
+                ></v-time-picker>
+              </v-menu>
+            </v-col>
+          </v-row>
+          <v-textarea
+              v-model="notes"
+              label="Notes"
+              counter
+              maxlength="200"
+          ></v-textarea>
+          <v-layout justify-end>
+            <v-btn class="mr-4"
+                   color="primary"
+                   @click="closeCreationDialog()"
+            >Save</v-btn>
+            <v-btn @click="cancelEventCreation()">Cancel</v-btn>
+          </v-layout>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
+import VueCal from 'vue-cal'
+import 'vue-cal/dist/vuecal.css'
+
 export default {
   name: 'CalendarAppointment',
   props: {
     msg: String
   },
-  data: () => ({
-    focus: '',
+  components: { VueCal },
+  data:vm => ({
     events: [],
-    colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
-    names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
-    categories: ['John Smith', 'Tori Walker', 'Benjamin Slates'],
-    mode: 'stack',
+    splitUsers: [
+      { id: 1, label: 'Dr 1' },
+      { id: 2, label: 'Dr 2' },
+      { id: 3, label: 'Dr 3' },
+      { id: 4, label: 'Dr 4' },
+      { id: 5, label: 'Dr 5' }
+    ],
+    selectedResource: [],
+    showEventCreationDialog: false,
+    selectedDate: null,
+    selectedEvent: null,
+    patientName: null,
+    nameRules: [
+      v => !!v || 'Patient Name is required',
+    ],
+    notes: null,
+
+    startDateMenu: false,
+    startDate: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+    startDateFormatted: vm.formatDate((new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10)),
+
+    endDateMenu: false,
+    endDate: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+    endDateFormatted: vm.formatDate((new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10)),
+
+    startTimeModel: false,
+    startTime: '09:00',
+
+    endTimeModel: false,
+    endTime: '09:00',
   }),
-  mounted () {
-    this.$refs.calendar.checkChange()
+  watch: {
+    startDate () {
+      this.startDateFormatted = this.formatDate(this.startDate)
+    },
+    endDate () {
+      this.endDateFormatted = this.formatDate(this.endDate)
+    },
   },
   methods: {
-    getEventColor (event) {
-      return event.color
-    },
-    setToday () {
-      this.focus = ''
-    },
-    prev () {
-      this.$refs.calendar.prev()
-    },
-    next () {
-      this.$refs.calendar.next()
-    },
-    fetchEvents ({ start, end }) {
-      const events = []
+    formatDate (date) {
+      if (!date) return null
 
-      const min = new Date(`${start.date}T00:00:00`)
-      const max = new Date(`${end.date}T23:59:59`)
-      const days = (max.getTime() - min.getTime()) / 86400000
-      const eventCount = this.rnd(days, days + 20)
+      const [year, month, day] = date.split('-')
+      return `${month}/${day}/${year}`
+    },
+    parseDate (date) {
+      if (!date) return null
 
-      for (let i = 0; i < eventCount; i++) {
-        const allDay = this.rnd(0, 3) === 0
-        const firstTimestamp = this.rnd(min.getTime(), max.getTime())
-        const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-        const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
-        const second = new Date(first.getTime() + secondTimestamp)
+      const [month, day, year] = date.split('/')
+      return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`
+    },
+    onEventCreate (event) {
+      this.selectedEvent = event
+      this.showEventCreationDialog = true
 
-        events.push({
-          name: this.names[this.rnd(0, this.names.length - 1)],
-          start: first,
-          end: second,
-          color: this.colors[this.rnd(0, this.colors.length - 1)],
-          timed: !allDay,
-          category: this.categories[this.rnd(0, this.categories.length - 1)],
+      return event
+    },
+    cancelEventCreation () {
+      this.showEventCreationDialog = false
+      this.selectedEvent = {}
+    },
+    closeCreationDialog () {
+      if (
+          this.startDate > this.endDate
+          || this.startTime > this.endTime
+          || !this.patientName
+      ) {
+        alert('Information incorrect!')
+      } else {
+        this.showEventCreationDialog = false
+        this.events.push({
+          start: this.startDate + ' ' + this.startTime,
+          end: this.endDate + ' ' + this.endTime,
+          title: this.patientName,
+          content: this.notes,
+          class: 'test-class',
+          split: this.selectedEvent.split
         })
+        this.selectedEvent = {}
       }
-
-      this.events = events
-    },
-    rnd (a, b) {
-      return Math.floor((b - a + 1) * Math.random()) + a
-    },
+    }
   },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
+  .vuecal__no-event {
+    display: none;
+  }
+  .vuecal__cell-split {
+    border-left: 1px solid hsla(0,0%,76.9%,.25);
+    background-color: rgba(255, 250, 196, 0.5);
+  }
+  .vuecal__event {
+    background-color: rgb(255, 255, 255);
+    border: 1px solid;
+    cursor: pointer;
+    user-select: none;
+  }
 </style>
